037bca7b5970228ea63f844559d63fbb52235bea
ff-forth <suks17@student.bth.se>
2025-04-15 22:13:03 +0200

    Refactor block_find and mem_alloc functions for clarity; added error handling and improved comments in memory_manager.c

--- memory_manager.c
+++ memory_manager.c
@@ -40,26 +40,26 @@ struct MemBlock* block_init(void* ptr, size_t size, void* next)
     return block;
 }
 
-// block_find finds the block and returns ptr of the searched block
+// block_find finds the block and returns ptr of the previous block
 struct MemBlock* block_find(void* block)
 {
     // Defind the block
-    struct MemBlock* mblock = &MemPool;
+    struct MemBlock* prevBlock = &MemPool;
 
-    // Find the block and remove
-    while (mblock->next != NULL)
+    // Find the block
+    while (prevBlock->next != NULL)
     {
-        if (mblock->next->ptr == block)
+        if (prevBlock->next->ptr == block)
         {
-            return mblock;
+            return prevBlock;
         }
     
         // Continue to the next block
-        mblock = mblock->next;
+        prevBlock = prevBlock->next;
     }
 
-    fprintf("block_find failed, can not find block %p in the memory pool.\n", block);
-    return mblock;
+    // fprintf(stderr, "block_find failed, can not find block %p in the memory pool.\n", block);
+    return prevBlock;
 };
 
 // mem_init initializes memory pool
@@ -67,12 +67,16 @@ void mem_init(size_t size)
 {
     // Allocate space in the memory
     void* ptr = malloc(size);
+    if (!ptr) 
+    {
+        fprintf(stderr, "mem_init failed, can not allocate memory.\n");
+        return;
+    }
 
     // Initialize MemPool
     MemPool.ptr = ptr;
     MemPool.size = size;
     MemPool.next = NULL;
-
 }
 
 // mem_alloc allocates space in the memory pool
@@ -81,29 +85,29 @@ void* mem_alloc(size_t size)
     // Check if size of MemBlock is greater than 0
     if (size <= 0)
     {
-        fprintf(stderr, "mem_alloc error: Too small, block size is %zu\n", size);
+        // fprintf(stderr, "mem_alloc error: Too small, block size is %zu\n", size);
         return NULL;
     }
 
     // Check if enough space in the Memory pool
     if (size > MemPool.size)
     {
-        fprintf(stderr, "mem_alloc error: Too large, Memory pool size is %zu.\n", MemPool.size);
+        // fprintf(stderr, "mem_alloc error: Too large, Memory pool size is %zu.\n", MemPool.size);
         return NULL;
     }
 
     // Check if Memory pool is empty
-    if (MemPool.next == NULL)
+    if (!MemPool.next)
     {
         MemPool.next = block_init(MemPool.ptr, size, NULL);
         return MemPool.next->ptr;
     }
 
     // Defind a block
-    struct MemBlock* block = MemPool.next;
+    struct MemBlock* currentBlock = MemPool.next;
     
-    // Check if it's space before the first block
-    if (block->ptr != MemPool.ptr && (MemPool.ptr + size) <= block->ptr)
+    // Check if it's a space before the first block
+    if (currentBlock->ptr != MemPool.ptr && (MemPool.ptr + size) <= currentBlock->ptr)
     {
         struct MemBlock *temp = MemPool.next;
         MemPool.next = block_init(MemPool.ptr, size, temp);
@@ -111,27 +115,27 @@ void* mem_alloc(size_t size)
     }
     
     // Check if it's space between two blocks
-    while (block->next != NULL)
+    while (currentBlock->next)
     {   
-        if ((block->ptr + block->size + size) <= block->next->ptr)
+        if ((currentBlock->ptr + currentBlock->size + size) <= currentBlock->next->ptr)
         {
-            struct MemBlock *temp = block->next;
-            block->next = block_init(block->ptr, size, temp);
-            return block->next->ptr;
+            struct MemBlock *temp = currentBlock->next;
+            currentBlock->next = block_init(currentBlock->ptr + currentBlock->size, size, temp);
+            return currentBlock->next->ptr;
         }
 
         // Move to the next block
-        block = block->next;
+        currentBlock = currentBlock->next;
     };
 
     // Check if it's enough space at the end
-    if ((block->ptr + block->size + size) <= (MemPool.ptr + MemPool.size) && (block->ptr + block->size) != 0)
+    if ((currentBlock->ptr + currentBlock->size + size) <= (MemPool.ptr + MemPool.size) && (currentBlock->ptr + currentBlock->size) != 0)
     {
-        block->next = block_init(block->ptr + block->size, size, NULL);
-        return block->next->ptr;
+        currentBlock->next = block_init(currentBlock->ptr + currentBlock->size, size, NULL);
+        return currentBlock->next->ptr;
     }
 
-    fprintf(stderr, "mem_alloc failed, can not allocate a space size %zu.\n", size);
+    // fprintf(stderr, "mem_alloc failed, can not allocate a space size %zu.\n", size);
     return NULL;
 }
 
@@ -139,33 +143,33 @@ void* mem_alloc(size_t size)
 void mem_free(void* block)
 {
     // Check if block ptr is null
-    if (block == NULL)
+    if (!block) 
     {
-        fprintf(stderr, "mem_free error: block ptr is null.\n");
+        // fprintf(stderr, "mem_free failed, block ptr is null.\n");
         return;
     }
 
-    // Check if memory pool is empty
-    if (MemPool.next == NULL)
+    // Check if MemPool is empty
+    if (!MemPool.next) 
     {
-        fprintf(stderr, "mem_free error: Memory pool is empty.\n");
+        // fprintf(stderr, "mem_free failed, MemPool is empty.\n");
         return;
     }
     
     // Defind the previous block to the block
-    struct MemBlock* mblock = block_find(block);
+    struct MemBlock* prevBlock = block_find(block);
 
     // Check if block exists
-    if (mblock->next == NULL)
-    {
-        fprintf(stderr, "mem_free failed, Block %p does not exist.\n");
-        return;
-    }
+    if (!prevBlock->next) return;
     
     // Remove the block
-    struct MemBlock *temp = mblock->next->next;
-    free(mblock->next);
-    mblock->next = temp;
+    struct MemBlock *temp = prevBlock->next->next;
+    free(prevBlock->next);
+    if (!prevBlock->next) 
+    {
+        fprintf(stderr, "mem_free failed, free the block %p failed.\n", block);
+    }
+    prevBlock->next = temp;
 
 }
 
@@ -184,6 +188,7 @@ void* mem_resize(void* block, size_t size)
     if (size <= msize)
     {
         mblock->size = size;
+        return block;
     }
     
     // Free the old block
@@ -206,9 +211,12 @@ void* mem_resize(void* block, size_t size)
 void mem_deinit()
 {
     // Free all mblock
-    while(MemPool.next != NULL)
+    struct MemBlock* mblock = MemPool.next;
+    while(mblock != NULL)
     {
-        mem_free(MemPool.next->ptr);
+        struct MemBlock* next = mblock->next;
+        free(mblock->ptr);
+        mblock = next;
     }
 
     // Free the pool
